28690caf3fd372fe9312fb70fcc5b68070f033b7
Oliver Hansen <oliv045r@edu.easj.dk>
2021-02-06 12:41:00 +0100

    COOKIES

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+document.getElementById("app").innerHTML = ``;
+window.cookieconsent.initialise({
+  container: document.getElementById("cookieconsent"),
+  palette:{
+    popup: { background: "#1aa3ff" },
+    button: { background: "#e0e0e0" },
+  },
+  revokable: true,
+  onStatusChange: function(status) {
+    console.log(this.hasConsented() ?
+    'enable cookies' : 'disable cookies');
+  },
+  "theme": "edgeless"
+});
+
